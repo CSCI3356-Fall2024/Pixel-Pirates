e1d46e6ff75d642d32113db2a2528ba4286ba532
anorazhu <zhusm@bc.edu>
2024-10-20 13:11:47 -0400

    changed the urls

--- Pixel_Pirates/urls.py
+++ Pixel_Pirates/urls.py
@@ -22,6 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('profile/', include('bc_sustain.urls')),
     path("", include("bc_sustain.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
