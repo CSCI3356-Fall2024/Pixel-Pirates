2baec2442f7caba2fe0894068cb9bb3e1a21ebeb
anorazhu <zhusm@bc.edu>
2024-10-20 13:09:39 -0400

    styling for the profile page

--- bc_sustain/models.py
+++ bc_sustain/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default='username')
     bc_email = models.EmailField(unique=True, default='default@bc.edu')
     school = models.CharField(max_length=100, choices=[
         ('CSOM', 'CSOM'),
@@ -20,6 +20,7 @@ class Profile(models.Model):
     minor1 = models.CharField(max_length=100, blank=True, null=True)
     minor2 = models.CharField(max_length=100, blank=True, null=True)
     picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    
+    bio = models.TextField(blank=True, null=True)
+
     def __str__(self):
-        return f"{self.user.username}'s Profile"
+        return f"{self.username.username}'s Profile"
