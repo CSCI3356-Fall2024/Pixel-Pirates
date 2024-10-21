77a093bb44f6c8e8593a9045fe0e85c53304b555
anorazhu <zhusm@bc.edu>
2024-10-21 15:59:41 -0400

    changed logout

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("profile/", views.ProfileView, name="profile"),
-    path("logout", views.logout_view)
+    path("", views.home, name="home"),
+    path("profile/", views.profile_view, name="profile"),
+     path("logout/", views.logout_view, name="logout")
 ]
