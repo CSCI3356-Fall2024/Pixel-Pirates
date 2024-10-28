2b3e8787c63094636624e6a6ad8b654a337fda43
Jason Cho <jcho3589@gmail.com>
2024-10-28 01:00:41 -0400

    added the migrations file, whenever a user attempts to sign in a non bc.edu, it deletes the user and makes them go back to the login page. The login page displays new text, telling the user to use an email that ends with @bc.edu

--- mainApp/signals.py
+++ mainApp/signals.py
@@ -1,6 +1,14 @@
 from allauth.account.signals import user_signed_up
-from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib import messages 
+from django.shortcuts import redirect 
+from django.core.exceptions import ValidationError
+from django.contrib.auth import logout 
+from allauth.exceptions import ImmediateHttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse 
+from django.db.models.signals import post_save
+
 from .models import Profile
 from django.contrib.auth.models import User
 
@@ -12,6 +20,12 @@ def create_profile_on_google_signup(request, user, **kwargs):
     if sociallogin:
         # Extract email and username from Google's OAuth response
         email = sociallogin.account.extra_data.get('email', '')
+
+        if not email.endswith('@bc.edu'):
+            user.delete()
+            logout(request)
+            messages.error(request, 'only email domain with @bc.edu are allowed')
+            raise ImmediateHttpResponse(redirect('login'))
         username_part = email.split('@')[0]  # Extract everything before '@'
 
         # Extract first and last names from Google's response
